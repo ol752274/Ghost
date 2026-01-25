514b86b17c3a7fa7fae8dd12f12980afdc09c08b
ol752274 <ol752274@gmail.com>
2026-01-25 13:06:35 +0530

    implement header

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+import Navitems from './Navitems'
+import UserDropdown from './UserDropdown'
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="StockSense Logo"
+            width={140}
+            height={32}
+          />
+        </Link>
+
+        <nav className="hidden sm:block">
+          <Navitems />
+        </nav>
+        <UserDropdown />
+      </div>
+    </header>
+  )
+}
+
+export default Header
