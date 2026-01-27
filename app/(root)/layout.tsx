0fa5b3070edd154f78daff2ee7364f2f1d327abb
ol752274 <ol752274@gmail.com>
2026-01-27 19:12:52 +0530

    implement authentication logic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import Header from '../../components/Header'
-const layout = ({children}:{children:React.ReactNode}) => {
+import {auth} from '@/lib/better-auth/auth'
+import { redirect } from 'next/navigation';
+import {headers} from "next/headers"
+const layout =async ({children}:{children:React.ReactNode}) => {
+  const session = await auth.api.getSession({headers:await headers()});
+  if(!session ?.user)redirect('/sign-in');
+  const user = {
+    id : session.user.id,
+    name:session.user.name,
+    email : session.user.email,
+
+
+  }
   return (
     <main className='min-h-screen text-gray-400'>
-        <Header />
+        <Header user={user}/>
         <div className='container py-10'>
             {children}
         </div>
